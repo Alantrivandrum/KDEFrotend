bb045cd2bd277eda04244233b2372700124450db
Nickson-0 <57009281+Nickson-0@users.noreply.github.com>
2022-11-20 14:30:23 +0000

    Added Queries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 
+let query_dict = {};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,15 +12,34 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    const url = "Placeholder for url of Graph DB API";
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => this.setState({ posts: json }));
+  componentDidMount(query_dict) {
+    let settings = { headers: { Accept: "application/sparql-results+json" } };
+    for (let i = 1; i < 10; i++) {
+      let query = "query_" + i;
+      const url = `http://localhost:7200/repositories/Test?query=${query_dict[query]}`;
+      fetch(url, settings)
+        .then((response) => response.json())
+        .then((json) => this.setState({ Kde_query_results: json }));
+    }
   }
 
   render() {
-    return <p>Hello world!</p>;
+    return (
+      <div>
+        <h1> Knowledge Graph</h1>
+        <span>{this.state.kde_query_results}</span>
+        <button>Query1</button>
+        <button>Query2</button>
+        <button>Query3</button>
+        <button>Query4</button>
+        <button>Query5</button>
+        <button>Query6</button>
+        <button>Query7</button>
+        <button>Query8</button>
+        <button>Query9</button>
+        <button>Query10</button>
+      </div>
+    );
   }
 }
 
